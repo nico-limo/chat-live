cf3e7c1fbc0d9d9c2402e79f6d98ee9b7dbe0c1a
nico-limo <nicolas.limonoff@gmail.com>
2024-02-27 17:16:09 -0300

    added my own code

--- server.ts
+++ server.ts
@@ -1,12 +1,34 @@
 const port = process.env.PORT || 3000;
 
-console.log(
-  `Launching Bun HTTP server on port: ${port}, url: http://0.0.0.0:${port} 🚀`
-);
-
-Bun.serve({
-  port: port,
-  fetch(_request) {
-    return new Response("Hello from Koyeb");
+const server = Bun.serve<WebSocketData>({
+  port,
+  fetch(req, server) {
+    server.upgrade(req, {
+      data: {
+        channelId: new URL(req.url).searchParams.get("channelId"),
+      },
+    });
+    return undefined;
+  },
+  websocket: {
+    open(ws) {
+      const chatId = ws.data.channelId;
+      console.log("Connection opened ", ws.data.channelId);
+      ws.subscribe(chatId);
+    },
+    close(ws) {
+      console.log("Connection close ", ws.data.channelId);
+    },
+    message(ws, message) {
+      console.log("Message sent ", ws.data.channelId);
+      const chatId = ws.data.channelId;
+      ws.publish(chatId, message);
+    },
   },
 });
+
+console.log(`Listening on Localhost: ${server.port}`);
+
+type WebSocketData = {
+  channelId: string;
+};
